78df5241d5c00e728ba94d755a77a351a7074d96
Johan Russouw <johanrussouw490@gmail.com>
2026-08-02 13:56:39 +0200

    successfully created first clerk email user

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,25 @@
-import { Image } from "expo-image";
-import { Text, View } from "react-native";
+import { Image } from 'expo-image';
+import { Link } from 'expo-router';
+import { Text, View } from 'react-native';
 
 export default function Index() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "orange"
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'black',
       }}
     >
-      <Text   className="text-blue-600 text-4xl">Edit app/index.tsx to edit this screen.</Text>
-      <Image source={require("../assets/images/icon.png")}
-      style={{ width: 200, height: 200, borderRadius: 20}}
+      <Text className="text-4xl text-blue-600">Edit app/index.tsx to edit this screen.</Text>
+      <Image
+        source={require('../assets/images/icon.png')}
+        style={{ width: 200, height: 200, borderRadius: 20 }}
       />
+      <Link className="text-3xl text-white" href={'/(auth)/sign-up'}>
+        Sign Up
+      </Link>
     </View>
   );
 }
